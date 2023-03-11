10b25421fbbdb94aae7554bad4a5c465df431dd5
Kapo1123 <kkpkenny13579@gmail.com>
2023-03-10 20:15:06 -0700

    Created index file

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
-class Game {
-    buttons;
-    allowPlayer;
-    sequence;
-    playerPlaybackPos;
-    mistakeSound;
-  
-    constructor() {
-      this.buttons = new Map();
-      this.allowPlayer = false;
-      this.sequence = [];
-      this.playerPlaybackPos = 0;
-      this.mistakeSound = loadSound('error.mp3');
-  
-      document.querySelectorAll('.game-button').forEach((el, i) => {
-        if (i < btnDescriptions.length) {
-          this.buttons.set(el.id, new Button(btnDescriptions[i], el));
-        }
-      });
-  
-      const playerNameEl = document.querySelector('.player-name');
-      playerNameEl.textContent = this.getPlayerName();
-    }
-    getPlayerName() {
-        return localStorage.getItem('userName') ?? 'Mystery player';
-      }
+function setplayer_name(){
+    let x= localStorage.getItem('userName') ?? 'Mystery player';
+
+  const playerNameEl = document.querySelector('.player-name');
+      playerNameEl.textContent = x;
+    } 
     
+setplayer_name();
+const addbutton = document.getElementsByClassName("form_button");
+addbutton[0].addEventListener("click",button_click,false);
+let b = 1;
+
+function button_click(e){
+    e.preventDefault();
+    const input_element = document.getElementsByClassName("input1")[0];
+    const table= document.getElementsByClassName("table1")[0];
+    const date = document.createElement("td");
+    const url = document.createElement("td");
+    url.style.textAlign = "center"; 
+    const button_row = document.createElement("td");
+    const newrow = document.createElement("tr");
+    const button = document.createElement("button");
+    button.textContent = "delect";
+    button.style.fontSize ="10px";
+    date.textContent =  "11/01/2023";
+    url.textContent = input_element.value;
+    button_row.appendChild(button);
+    newrow.appendChild(date);
+    newrow.appendChild(url);
+    newrow.appendChild(button_row);
+    table.appendChild(newrow);
+    newrow.id ="table"+b;
+    b++;
 }
+// function 
+    
   
